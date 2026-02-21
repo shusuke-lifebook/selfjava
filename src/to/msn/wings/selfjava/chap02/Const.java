28918cc0a960f896da8f39901fe22bae94e4d090
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-02-21 16:41:20 +0900

    Chap02の学習開始

---
+++ src/to/msn/wings/selfjava/chap02/Const.java
@@ -0,0 +1,10 @@
+package to.msn.wings.selfjava.chap02;
+
+public class Const {
+    public static void main(String[] args) {
+        final Double TAX = 1.1;
+        int price = 1000;
+        double sum = price * TAX;
+        System.out.println(sum);
+    }
+}
